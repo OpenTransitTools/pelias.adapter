ea0f659b58a14b5df6aa0c4f3a307fd67a66353a
Frank Purcell <fxpurcell@gmail.com>
2018-02-12 12:46:20 -0800

    initial add 4/?

--- pelias/adapter/tests/tests.py
+++ pelias/adapter/tests/tests.py
@@ -5,7 +5,7 @@ import urllib
 import contextlib
 import json
 
-PORT="44444"
+PORT="45454"
 
 
 class TestMyView(unittest.TestCase):
@@ -15,78 +15,11 @@ class TestMyView(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_routes(self):
-        url = get_url('routes')
+    def test_solr(self):
+        url = get_url('solr', 'limit=30&query=2')
         j = call_url(url)
         s = json.dumps(j)
         self.assertEqual(j['status_code'], 200)
-        self.assertGreaterEqual(j['count'], 85)
-        self.assertRegexpMatches(s,"MAX Blue")
-
-    def test_route(self):
-        url = get_url('route', 'route_id=100')
-        j = call_url(url)
-        s = json.dumps(j)
-        self.assertEqual(j['status_code'], 200)
-        self.assertRegexpMatches(s,"MAX Blue")
-
-    def test_rs(self):
-        def asserts(j, s):
-            self.assertEqual(j['status_code'], 200)
-            self.assertGreaterEqual(j['directions'][0]['stop_list']['count'], 45)
-            self.assertRegexpMatches(s,"MAX Blue")
-            self.assertRegexpMatches(s,"Hatfield Government")
-
-        # both route directions
-        url = get_url('route_stops', 'route_id=100')
-        j = call_url(url)
-        s = json.dumps(j)
-        asserts(j, s)
-        self.assertGreaterEqual(len(j['directions']), 2)
-
-        # single route direction
-        url = get_url('route_stops', 'route_id=100&direction_id=1')
-        j = call_url(url)
-        s = json.dumps(j)
-        asserts(j, s)
-        self.assertGreaterEqual(len(j['directions']), 1)
-
-    def test_stop(self):
-        url = get_url('stop', 'stop_id=2')
-        j = call_url(url)
-        s = json.dumps(j)
-        self.assertEqual(j['status_code'], 200)
-        self.assertRegexpMatches(s,"Lake Oswego")
-
-    def test_stops_near(self):
-        url = get_url('stops_near', 'lat=45.5&lon=-122.5')
-        j = call_url(url)
-        s = json.dumps(j)
-        self.assertEqual(j['status_code'], 200)
-        self.assertEqual(j['count'], 10)
-        self.assertRegexpMatches(s,"SE Division")
-
-    def test_stop_schedule(self):
-        url = get_url('stop_schedule', 'stop_id=2')
-        j = call_url(url)
-        s = json.dumps(j)
-        self.assertEqual(j['status_code'], 200)
-        self.assertRegexpMatches(s,"Lake Oswego")
-
-    def test_plan_trip(self):
-        url = get_url('plan_trip', 'from=pdx::45.587546,-122.592925&to=zoo')
-        j = call_url(url)
-        s = json.dumps(j)
-        self.assertRegexpMatches(s,"Zoo")
-        self.assertRegexpMatches(s,"itineraries")
-
-    def test_geocode(self):
-        url = get_url('geocode', 'place=zoo')
-        j = call_url(url)
-        s = json.dumps(j)
-        self.assertEqual(j['status_code'], 200)
-        self.assertRegexpMatches(s,"-122.71")
-        self.assertRegexpMatches(s,"45.51")
 
 
 def get_url(svc_name, params=None):
@@ -95,11 +28,13 @@ def get_url(svc_name, params=None):
         ret_val = "{0}?{1}".format(ret_val, params)
     return ret_val
 
+
 def call_url(url):
     print url
     with contextlib.closing(urllib.urlopen(url)) as f:
         ret_json = json.load(f)
     return ret_json
+
 
 def call_url_text(url):
     print url

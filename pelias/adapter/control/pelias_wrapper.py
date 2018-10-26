68911caac18d498ea4f518aef2deb56a98599cde
Frank Purcell <fxpurcell@gmail.com>
2018-10-25 21:40:10 -0700

    lat lon parsing

--- pelias/adapter/control/pelias_wrapper.py
+++ pelias/adapter/control/pelias_wrapper.py
@@ -1,5 +1,6 @@
 from ott.utils.svr.pyramid import response_utils
 from ott.utils import html_utils
+from ott.utils import geo_utils
 from . import pelias_json_queries
 
 class PeliasWrapper(object):
@@ -7,17 +8,30 @@ class PeliasWrapper(object):
     @classmethod
     def wrapp(cls, main_url, bkup_url, reverse_geo_url, query_string, def_size=5):
         """ will call either autocomplete or search """
-        # import pdb; pdb.set_trace()
+        ret_val = None
+
+        import pdb; pdb.set_trace()
 
         # step 1: break out the size and text parameters
-        text = html_utils.get_param_value_from_qs(query_string, 'text');
-        size = html_utils.get_numeric_value_from_qs(query_string, 'size', def_size);
+        size = html_utils.get_numeric_value_from_qs(query_string, 'size', def_size)
+        text = html_utils.get_param_value_from_qs(query_string, 'text')
 
-        ret_val = response_utils.proxy_json(main_url, query_string)
-        if ret_val is None or ret_val['features'] is None or len(ret_val['features']) < 1:
-            ret_val = response_utils.proxy_json(bkup_url, query_string)
+        # step 2 call reverse geocoder if we think text is a coord
+        if geo_utils.is_coord(text):
+            x, y = geo_utils.ll_from_str(text)
+            ll = geo_utils.xy_to_url_param_str(x, y, x_name="point.lon", y_name="point.lat", check_lat_lon=True)
+            qs = "{}&{}".format(query_string, ll)
+            ret_val = response_utils.proxy_json(reverse_geo_url, qs)
 
+        # step 3: call geocoder (if we didn't already reverse geocode, or if that result was null)
+        if ret_val is None:
+            ret_val = response_utils.proxy_json(main_url, query_string)
+            if ret_val is None or ret_val['features'] is None or len(ret_val['features']) < 1:
+                ret_val = response_utils.proxy_json(bkup_url, query_string)
+
+        # step 4: clean up the label attribute
         cls.fixup_response(ret_val, size)
+
         return ret_val
 
     @classmethod
@@ -27,11 +41,12 @@ class PeliasWrapper(object):
         # step 1: loop thru the records in the Pelias response
         if pelias_json.get('features'):
             for i, f in enumerate(pelias_json['features']):
+                if i >= size:
+                    break
+
                 p = f.get('properties')
                 if p is None:
                     continue
-                if i > size:
-                    break
 
                 rename = None
 

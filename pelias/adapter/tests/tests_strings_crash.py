1fdeb82eb862d559607e7727bd5f544b3f6699c1
Frank Purcell <fxpurcell@gmail.com>
2018-10-31 01:40:41 -0700

    test work

--- pelias/adapter/tests/tests_strings_crash.py
+++ pelias/adapter/tests/tests_strings_crash.py
@@ -22,10 +22,17 @@ class TestStringsCrash(BaseUnit):
             res = self.call_test_json(url, find_attribute="features")
             self.assertTrue(res)
 
-    def xtest_alias(self):
+    def test_alias(self):
         csv = self.csv('./data/alias.csv', "NAME")
         for c in csv:
             url = self.url_tmpl + c.get("NAME")
+            res = self.call_test_json(url, find_attribute="features")
+            self.assertTrue(res)
+
+    def test_stops(self):
+        csv = self.csv('./data/stops.csv', "stop_code")
+        for c in csv:
+            url = self.url_tmpl + c.get("stop_code")
             res = self.call_test_json(url, find_attribute="features")
             self.assertTrue(res)
 

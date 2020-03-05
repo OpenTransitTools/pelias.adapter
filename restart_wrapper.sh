378d2d9e3f75aaa3fc5c4cbf26277f0330c3f35d
Frank Purcell <fxpurcell@gmail.com>
2020-03-05 12:57:36 -0800

    fix(restart script): flesh out the wrapper restart script

--- restart_wrapper.sh
+++ restart_wrapper.sh
@@ -1,1 +1,22 @@
+# step 1: get into the wrapper directory
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+# step 2: force get latest and greatest code
+git reset --hard HEAD
+git pull
+
+# step 3: remove (potentially) outdated dependencies and buildout to get latest / greatest
+RM_DEPS="rm -rf *egg*/ott.utils"
+echo $RM_DEPS
+eval $RM_DEPS
+
+RM_JUNK="rm -f nohup.out *~"
+echo $RM_JUNK
+eval $RM_JUNK
+
+buildout
+
+# step 4: stop & restart wrapper
 pkill -9 -f -c local_pelias
+nohup bin/pserve config/local_pelias.ini &

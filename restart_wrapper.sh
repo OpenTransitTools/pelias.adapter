546f13738612ef874d6a0eb688b183467d6c4fee
Frank Purcell <fxpurcell@gmail.com>
2020-05-22 12:28:44 -0700

    fix(scripts): disown pelias startup try 2

--- restart_wrapper.sh
+++ restart_wrapper.sh
@@ -7,18 +7,16 @@ git reset --hard HEAD
 git pull
 
 # step 3: remove (potentially) outdated dependencies and buildout to get latest / greatest
-RM_DEPS="rm -rf *egg*/ott.utils"
+RM_DEPS="rm -rf *egg*/ott.utils* nohup.out *~"
 echo $RM_DEPS
 eval $RM_DEPS
-
-RM_JUNK="rm -f nohup.out *~"
-echo $RM_JUNK
-eval $RM_JUNK
-
 buildout
 
 # step 4: stop & restart wrapper
 pkill -9 -f -c local_pelias
-nohup bin/pserve config/local_pelias.ini & disown
+cmd="bin/pserve config/local_pelias.ini"
+eval "${cmd}" &>/dev/null & disown;
+
+# step 5: really done hopefully...
 echo 
 echo "DONE"

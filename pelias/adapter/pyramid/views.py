6900ce70dbf6b02346555a2838f4237aeb8c95e1
Alice <martiali@trimet.org>
2025-11-13 15:48:05 -0800

    refine method

--- pelias/adapter/pyramid/views.py
+++ pelias/adapter/pyramid/views.py
@@ -1,16 +1,13 @@
+import logging
+
+from ott.utils import object_utils
+from ott.utils.svr.pyramid import globals
+from ott.utils.svr.pyramid import response_utils
+from pelias.adapter.control.pelias_to_solr import PeliasToSolr
+from pelias.adapter.service import refine_service, pelias_service
 from pyramid.view import view_config
 
-from ott.utils import json_utils
-from ott.utils import object_utils
-from ott.utils.svr.pyramid import response_utils
-from ott.utils.svr.pyramid import globals
-
-from pelias.adapter.control.pelias_to_solr import PeliasToSolr
-from pelias.adapter.control.pelias_wrapper import PeliasWrapper
-
-import logging
 log = logging.getLogger(__file__)
-
 
 # urls
 pelias_autocomplete_url = None
@@ -42,6 +39,7 @@ def do_view_config(cfg):
     cfg.add_route('pelias_rtp', '/pelias/rtp/{service}')
     cfg.add_route('solr', '/solr')
     cfg.add_route('solr_select', '/solr/{select}')
+    cfg.add_route('pelias_refine', '/pelias/refine/{select}')
 
 
 @view_config(route_name='solr', renderer='json', http_cache=globals.CACHE_LONG)
@@ -99,7 +97,7 @@ def solr_select(request):
 
 
 @view_config(route_name='pelias_services', renderer='json', http_cache=globals.CACHE_LONG)
-def pelias_services(request, is_rtp=False):
+def pelias_services(request, is_rtp=False, refine=False):
     """
     calls pellias wrapper based on specified service (autocomplete == default, search or reverse)
     :return: json data from Pelias ... after fixing up the response in ways defined by 'PeliasWrapper'
@@ -107,32 +105,16 @@ def pelias_services(request, is_rtp=False):
     # import pdb; pdb.set_trace()
 
     # step 1: find the service based on pelias/{service}? ... default to autocomplete
+
     try:
         service = request.matchdict['service']
     except:
         service = "autocomplete"
 
-    # step 2: call the wrapper
-    if service == "autocomplete":
-        ret_val = PeliasWrapper.wrapp(pelias_autocomplete_url, pelias_search_url, pelias_reverse_url, request.query_string, is_rtp=is_rtp)
-    elif service == "search":
-        ret_val = PeliasWrapper.wrapp(pelias_search_url, pelias_autocomplete_url, pelias_reverse_url, request.query_string, is_rtp=is_rtp)
-    elif service == "reverse":
-        ret_val = PeliasWrapper.reverse(pelias_reverse_url, request.query_string)
-    else:
-        ret_val = response_utils.sys_error_response()
+    if refine:
+        return refine_service.refine(request=request, service=service)
 
-    # step 3: append the hostname to the response
-    json_utils.append_hostname_to_json(ret_val)
-
-    """
-    TODO: WIP find and add stops / in/out / etc... 
-    x = object_utils.find_elements('properties', ret_val)
-    for z in x:
-        z["XXXX"] = 'MMMMMMMMMMM'
-    import pdb; pdb.set_trace()
-    """
-    return ret_val
+    return pelias_service.get_pelias_response(request=request, service=service, is_rtp=is_rtp)
 
 
 @view_config(route_name='pelias', renderer='json', http_cache=globals.CACHE_LONG)
@@ -140,6 +122,12 @@ def pelias_services(request, is_rtp=False):
 def pelias(request):
     """ call pelias_services() above w/out specifying a service ... so will default to autocomplete """
     return pelias_services(request)
+
+
+@view_config(route_name='pelias_refine', renderer='json', http_cache=globals.CACHE_LONG)
+def pelias_refine(request):
+    """ call pelias_services() above w/out specifying a service ... so will default to autocomplete """
+    return pelias_services(request, refine=True)
 
 
 @view_config(route_name='pelias_rtp', renderer='json', http_cache=globals.CACHE_LONG)
---
+++ pelias/adapter/service/pelias_service.py
@@ -0,0 +1,32 @@
+from typing import Literal, Any
+
+from ott.utils import json_utils
+from ott.utils.svr.pyramid import response_utils
+from pelias.adapter.control.pelias_wrapper import PeliasWrapper
+from pyramid.request import Request
+
+
+def get_pelias_response(service:Literal["autocomplete", "search", "reverse"], request:Request, is_rtp:bool=False) -> dict[str, Any]:
+    from pelias.adapter.pyramid.views import pelias_autocomplete_url, pelias_reverse_url, pelias_search_url
+
+    # step 2: call the wrapper
+    if service == "autocomplete":
+        ret_val = PeliasWrapper.wrapp(pelias_autocomplete_url, pelias_search_url, pelias_reverse_url, request.query_string, is_rtp=is_rtp)
+    elif service == "search":
+        ret_val = PeliasWrapper.wrapp(pelias_search_url, pelias_autocomplete_url, pelias_reverse_url, request.query_string, is_rtp=is_rtp)
+    elif service == "reverse":
+        ret_val = PeliasWrapper.reverse(pelias_reverse_url, request.query_string)
+    else:
+        ret_val = response_utils.sys_error_response()
+
+    # step 3: append the hostname to the response
+    json_utils.append_hostname_to_json(ret_val)
+
+    """
+    TODO: WIP find and add stops / in/out / etc... 
+    x = object_utils.find_elements('properties', ret_val)
+    for z in x:
+        z["XXXX"] = 'MMMMMMMMMMM'
+    import pdb; pdb.set_trace()
+    """
+    return ret_val
